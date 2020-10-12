721e3c65c64df1556174426b9cb26bf288b607f0
tan <ttru702@gmail.com>
2020-10-12 17:58:32 +0900

    method: click all next button

--- load_urls.py
+++ load_urls.py
@@ -2,10 +2,11 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://www.google.com/')
-time.sleep(5)
-search_box = driver.find_element_by_name("q")
-search_box.send_keys('ChromeDriver')
-search_box.submit()
+driver.get('https://keizaiclub.com/category/%e8%bf%91%e6%9c%aa%e6%9d%a5%e4%ba%88%e6%b8%ac/')
+
+while driver.find_element_by_css_selector('#load_post .next') != 0:
+  btn_nuxt = driver.find_element_by_css_selector('#load_post .next')
+  btn_nuxt.click()
+  time.sleep(5)
 time.sleep(5)
 driver.quit()
