e8c155a4ba1a98aaafd3b039f300f47dfe2a98a0
tan <ttru702@gmail.com>
2020-10-12 18:22:44 +0900

    success link and title

--- load_urls.py
+++ load_urls.py
@@ -2,11 +2,20 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://keizaiclub.com/category/%e8%bf%91%e6%9c%aa%e6%9d%a5%e4%ba%88%e6%b8%ac/')
 
-while driver.find_element_by_css_selector('#load_post .next') != 0:
-  btn_nuxt = driver.find_element_by_css_selector('#load_post .next')
-  btn_nuxt.click()
-  time.sleep(5)
-time.sleep(5)
+url_base = 'https://keizaiclub.com/category/%e8%bf%91%e6%9c%aa%e6%9d%a5%e4%ba%88%e6%b8%ac/page/'
+page_number = 57
+
+while True:
+  url = f'{url_base}{page_number}/'
+  driver.get(url)
+  time.sleep(3)
+  if driver.title != '近未来予測に関する記事一覧':
+    break
+  articles = driver.find_elements_by_css_selector('#ajax_load_post_list article .title a')
+  for art in articles:
+    print(art.get_attribute('href'))
+    print(art.get_attribute('title'))
+  page_number += 1
+
 driver.quit()
