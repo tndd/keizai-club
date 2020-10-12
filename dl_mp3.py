730490fa402a36298c22861875a78bf96e974900
tan <ttru702@gmail.com>
2020-10-13 00:29:42 +0900

    change to load all files

--- dl_mp3.py
+++ dl_mp3.py
@@ -1,25 +1,27 @@
 import sys
 import os
 import json
+import glob
 # from selenium import webdriver
 
 # driver = webdriver.Chrome()
 
-dl_target = sys.argv[1]
+dl_targets = glob.glob('loaded_urls/*.tsv')
 
 dl_dir_name = 'downloaded'
 os.makedirs(dl_dir_name, exist_ok=True)
 
-with open(f'./loaded_urls/{dl_target}.tsv') as f:
-  lines = list(map(lambda x: x.strip().split('\t'), f.readlines()))
-
 progress = dict()
-for l in lines:
-  progress[l[1]] = {
-    'group': dl_target,
-    'name': l[2],
-    'status': False
-  }
+for dl_target in dl_targets:
+  with open(dl_target) as f:
+    lines = list(map(lambda x: x.strip().split('\t'), f.readlines()))
+
+  for l in lines:
+    progress[l[1]] = {
+      'group': dl_target,
+      'name': l[2],
+      'status': False
+    }
 
 with open('dl_progress.json', 'w') as f:
   json.dump(progress, f, indent=2)
