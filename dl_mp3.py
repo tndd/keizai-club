1a10152e5c618f06b0a6827f48576d3e3c6e8545
tan <ttru702@gmail.com>
2020-10-13 14:14:41 +0900

    wip: downloader

--- dl_mp3.py
+++ dl_mp3.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import json
 import glob
@@ -10,9 +9,8 @@ from selenium import webdriver
 progress_file_name = 'dl_progress.json'
 download_dir_name = 'downloaded'
 
-def init_dl_progress(dl_dir_name, prog_file_name):
+def init_dl_progress(prog_file_name):
   dl_targets = glob.glob('loaded_urls/*.tsv')
-  os.makedirs(dl_dir_name, exist_ok=True)
 
   progress = dict()
   for dl_target in dl_targets:
@@ -24,8 +22,9 @@ def init_dl_progress(dl_dir_name, prog_file_name):
         'name': l[2],
         'status': False
       }
-  with open('dl_progress.json', 'w') as f:
+  with open(prog_file_name, 'w') as f:
     json.dump(progress, f, indent=2)
+  print(f"Create progress file: {progress_file_name}")
 
   return progress
 
@@ -47,13 +46,36 @@ time.sleep(1)
 driver.find_element_by_css_selector('.swpm-login-form-submit').click()
 
 # download files
-for url, detail in progress.items():
-  if detail['status'] == True:
-    continue
-  driver.get(url)
-  links = driver.find_elements_by_css_selector('a')
-  for l in links:
-    if l.text == '音声ダウンロード（MP3)':
-      urllib.request.urlretrieve(l.get_attribute('href'), detail['name'])
-      time.sleep(1)
-      break
+try:
+  for url, detail in progress.items():
+    if detail['status'] == True:
+      continue
+    # tmp
+    if detail['group'] != 'podcasting':
+      continue
+    driver.get(url)
+    time.sleep(1)
+    links = driver.find_elements_by_css_selector('a')
+    for l in links:
+      if l.text == '音声ダウンロード（MP3)':
+        print(f"Downloading: {url}")
+        # extract date num
+        urls = url.split('/')
+        date_num = urls[-4] + urls[-3] + urls[-2]
+        file_name = f"{date_num}_{detail['name']}.mp3"
+        # create download dir
+        group_dir_path = f"./{download_dir_name}/{detail['group']}"
+        os.makedirs(group_dir_path, exist_ok=True)
+        # download mp3
+        urllib.request.urlretrieve(l.get_attribute('href'), f"{group_dir_path}/{file_name}")
+        progress[url]['status'] = True
+        print(f"completed: {file_name}")
+        time.sleep(1)
+        break
+except Exception as e:
+  print(e)
+finally:
+  # save progress
+  with open(progress_file_name, 'w') as f:
+    json.dump(progress, f, indent=2)
+  print(f"Updated: {progress_file_name}")
