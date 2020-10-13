d0644f43d19f64592e07fcc13c32c56a11f9ff51
tan <ttru702@gmail.com>
2020-10-13 19:47:57 +0900

    separate load url function

--- load_urls.py
+++ load_urls.py
@@ -1,33 +1,42 @@
 import time
-import sys
 import os
 from selenium import webdriver
 
 driver = webdriver.Chrome()
 
-target = sys.argv[1]
-target_en = sys.argv[2]
+def load_links(target, target_en):
+  url_base = f'https://keizaiclub.com/category/{target}/page/'
+  progress_dir_name = 'loaded_urls'
+  progerss_file_path = f'./{progress_dir_name}/{target_en}.tsv'
+  os.makedirs(progress_dir_name, exist_ok=True)
 
-url_base = f'https://keizaiclub.com/category/{target}/page/'
-progress_dir_name = 'loaded_urls'
-progerss_file_path = f'./{progress_dir_name}/{target_en}.tsv'
+  # init link file
+  with open(progerss_file_path, 'w') as f:
+    f.write('')
+  print(f"init: {progerss_file_path}")
 
-page_number = 1
+  page_number = 1
+  while True:
+    url = f'{url_base}{page_number}/'
+    driver.get(url)
+    time.sleep(3)
+    if 'ページが見つかりませんでした' in driver.title:
+      print(f'QUALIFIED: {target}')
+      break
+    articles = driver.find_elements_by_css_selector('#ajax_load_post_list article .title a')
+    for art in articles:
+      link = art.get_attribute('href')
+      title = art.get_attribute('title')
+      with open(progerss_file_path, 'a') as f:
+        f.write(f'{page_number}\t{link}\t{title}\n')
+    page_number += 1
 
-os.makedirs(progress_dir_name, exist_ok=True)
+# load targets
+with open('target_list.tsv', 'r') as f:
+  lines = list(map(lambda x: x.strip().split('\t'), f.readlines()))
 
-while True:
-  url = f'{url_base}{page_number}/'
-  driver.get(url)
-  time.sleep(3)
-  if 'ページが見つかりませんでした' in driver.title:
-    print(f'QUALIFIED: {target}')
-    break
-  articles = driver.find_elements_by_css_selector('#ajax_load_post_list article .title a')
-  for art in articles:
-    link = art.get_attribute('href')
-    title = art.get_attribute('title')
-    with open(progerss_file_path, 'a') as f:
-      f.write(f'{page_number}\t{link}\t{title}\n')
-  page_number += 1
+# execute load from lines
+for l in lines:
+  load_links(l[0], l[1])
+
 driver.quit()
